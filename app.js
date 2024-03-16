e883ddb57a5ccbe86d0d67d0c6db94a76a311f3e
emre <emre>
2024-03-16 17:48:56 +0300

    tür dönüşümü

---
+++ app.js
@@ -0,0 +1,46 @@
+/*
+            TÜR DÖNÜŞÜMLERİ
+stirngs number booleans undefined and null
+object function
+
+*/ 
+
+
+//String veri tipinden number veri tipine dönüştürmek..
+
+// let a = 5;
+// let b = Number("10");
+
+// console.log(a+b);
+
+
+// let c = Number(b);
+// console.log(a+c);
+
+//parseInt()
+
+// let a = 8;
+// let b = parseInt("66.8"); // virgül sonrasını atar
+// console.log(b);
+
+// let c = parseFloat("12.7"); //virgül sonrasını da yazdırır
+// console.log(c);
+
+//NUMBER VERİ TİPİNDEN STRİNG VERİ TİPİNE DÖNÜŞTÜRME
+
+//let x = String(66);
+// let x =(66).toString();
+// console.log(typeof x);
+// console.log(x);
+
+//Boolean veri tipini stringe dönüştürme
+
+// let sonuc = String(true);
+// console.log(typeof sonuc);
+// console.log(sonuc);
+
+//object tipini stringe çevirme
+
+let rakamlar = String([1,2,3,4,5]);
+console.log(typeof rakamlar);
+console.log(rakamlar);
